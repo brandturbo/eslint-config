0a7b02df1cd629215632cc78af15166166757440
Levin Keller <post@levinkeller.de>
2022-09-29 07:54:30 +0200

    prepare for publish

--- lib/index.ts
+++ lib/index.ts
@@ -11,3 +11,38 @@ import requireIndex  from 'requireindex'
 
 // import all rules in lib/rules
 export const rules = requireIndex(__dirname + '/rules')
+export const configs =
+    {
+        "recommended": {
+
+            root: true,
+            parser: '@typescript-eslint/parser',
+            plugins: [
+                '@typescript-eslint',
+                'simple-import-sort',
+                'functional',
+                '@fanclub',
+            ],
+            extends: [
+                'eslint:recommended',
+                'plugin:@typescript-eslint/recommended',
+                'plugin:functional/external-recommended',
+                'plugin:functional/no-object-orientation',
+                'plugin:unicorn/recommended',
+                'prettier',
+            ],
+            env: {
+                node: true,
+            },
+            rules: {
+                'simple-import-sort/imports': 'error',
+                'simple-import-sort/exports': 'error',
+                'arrow-body-style': ['warn', 'as-needed'],
+                '@typescript-eslint/ban-ts-comment': 'error',
+                'no-console': 'error',
+                '@typescript-eslint/no-unused-vars': 'error',
+                'unicorn/filename-case': ['error', {case: 'camelCase'}],
+                '@fanclub/no-array-methods': 'error',
+            },
+        }
+    }
